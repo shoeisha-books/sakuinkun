a603eaca36fbafa5e02eacee1b7f1313f2d8b096
satoshi yamamoto <s-yamamoto@shoeisha.co.jp>
2025-04-14 15:39:47 +0900

    cpp sample app(temp)

--- cli_app_cpp/src/main.cpp
+++ cli_app_cpp/src/main.cpp
@@ -1,6 +1,10 @@
-#include "libsakuin.h"
+#include "sakuinkunpp/SakuinKun.hpp"
 int main(int argc, char* argv[])
 {
+    auto helpMessage =
+R"(さくいんくん
+cmd: sakuin-clipp )";
     // コンソールアプリもそのうち作る
+    sakuin::SakuinKun::getInstance().initialize();
     return 0;
 }
